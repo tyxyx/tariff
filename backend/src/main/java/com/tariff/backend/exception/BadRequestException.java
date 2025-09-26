7e6b2f6a3329af7b9506ec5d212f1d51b2b5b6a2
Rayner3103 <80162238+Rayner3103@users.noreply.github.com>
2025-09-26 13:59:44 +0800

    Added exception classes

--- backend/src/main/java/com/tariff/backend/exception/BadRequestException.java
+++ backend/src/main/java/com/tariff/backend/exception/BadRequestException.java
@@ -3,7 +3,7 @@ package com.tariff.backend.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-// Custom exception class for handling resource not found scenario
+// throw when the request body does not fit the criteria / when the user is trying to do something illegal
 @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Bad request")
 public class BadRequestException extends RuntimeException {
 
