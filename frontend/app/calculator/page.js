089ab81bc3333a2f5f60413ab3c7497ee2ae478c
wzinl <waizinln3@gmail.com>
2025-09-29 16:45:20 +0800

    Removed reedundant build for ARM64 on CICD workflow

--- frontend/app/calculator/page.js
+++ frontend/app/calculator/page.js
@@ -66,7 +66,7 @@ export default function CalculatorPage() {
     ) {
       // Format date as YYYY-MM-DD
       const formattedDate = calculationDate.toISOString().split("T")[0];
-      fetch("http://localhost:8080/api/tariffs/get-particular-tariff-rate", {
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tariffs/get-particular-tariff-rate`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
