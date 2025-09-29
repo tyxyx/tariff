fade47ea80ea77cb3f087421b84803036ea1cda8
wzinl <waizinln3@gmail.com>
2025-09-29 17:00:24 +0800

    Added ellipsis to frontend title

--- backend/src/main/java/com/tariff/backend/repository/UserRepository.java
+++ backend/src/main/java/com/tariff/backend/repository/UserRepository.java
@@ -3,12 +3,11 @@ package com.tariff.backend.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.tariff.backend.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
   Optional<User> findByEmail(String email);
 }
