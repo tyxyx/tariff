c39fb6c11f9b0e5db5e081bd5eae4aaf3da634f1
wzinl <waizinln3@gmail.com>
2025-11-13 18:57:49 +0800

    auth debugging on prod

--- backend/src/main/java/com/tariff/backend/controller/UserController.java
+++ backend/src/main/java/com/tariff/backend/controller/UserController.java
@@ -1,21 +1,29 @@
 package com.tariff.backend.controller;
 
-import com.tariff.backend.dto.UserLoginDTO;
-import com.tariff.backend.dto.UserRequestDTO;
-import com.tariff.backend.model.User;
-import com.tariff.backend.service.JwtService;
-import com.tariff.backend.service.UserService;
-import jakarta.validation.Valid;
 import java.util.List;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tariff.backend.dto.UserLoginDTO;
+import com.tariff.backend.dto.UserRequestDTO;
+import com.tariff.backend.model.User;
+import com.tariff.backend.service.JwtService;
+import com.tariff.backend.service.UserService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(path = "api/users")
@@ -37,6 +45,7 @@ public class UserController {
   //   Return users with token for testing
   @GetMapping("/me")
   public ResponseEntity<User> authenticatedUser() {
+    System.out.println( "Fetching authenticated user" );
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     User currentUser = (User) authentication.getPrincipal();
     return ResponseEntity.ok(currentUser);
